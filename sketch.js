1063493117d85ce9e0403b09f2c6cae75129a817
tabo <1005893565@u.icesi.edu.com>
2022-10-13 01:35:45 -0500

    add movement function and test objects

--- sketch.js
+++ sketch.js
@@ -1,3 +1,6 @@
+let player;
+let objTest;
+
 function preload() {
   background = loadImage("./assets/background.png");
   beerObject = loadImage("./assets/beer.png");
@@ -11,11 +14,41 @@ function preload() {
 function setup() {
   createCanvas(1277, 639);
   //imageMode(CORNER);
+  player = new Player(0, 0, characterObject);
+  objTest = new Obj(50, 50, 40, 40);
 }
 
 function draw() {
   image(background, 0, 0);
   
-  let characterImg = image(characterObject[0], 0, 0);
+  player.draw();
+  objTest.draw();
+  
+
+	// test
+  if (player.hit(objTest)){
+	  fill(0,0,200);
+	  rect(200,200,100,100);
+	  console.log("ses")
+  }
 }
 
+function keyPressed() {
+	switch (keyCode){
+		case 87:
+			player.move(0);
+			break;
+
+		case 83:
+			player.move(1);
+			break;
+
+		case 65:
+			player.move(2);
+			break;
+
+		case 68:
+			player.move(3);
+			break;
+	}
+}
