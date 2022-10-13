416baa2190dad5669b9bc1c4c5d864f2c066eb76
apovedq <povedandres11@gmail.com>
2022-10-12 23:03:53 -0500

    Added assets and loaded images

--- sketch.js
+++ sketch.js
@@ -1,12 +1,21 @@
-// Game
-let test
+function preload() {
+  background = loadImage("./assets/background.png");
+  beerObject = loadImage("./assets/beer.png");
+  meatObject = loadImage("./assets/meat.png");
+  // Pos 0 = right, 1 = left, 2= back
+  managerObject = [loadImage("./assets/manager_right.png"), loadImage("./assets/manager_left.png"), loadImage("./assets/manager_back.png")];
+  // Pos 0 = right, 1 = left, 2= back
+  characterObject = [loadImage("./assets/woman_right.png"), loadImage("./assets/woman_left.png"), loadImage("./assets/woman_back.png")];
+}
 
 function setup() {
-	test = "works";
-
+  createCanvas(1277, 639);
+  //imageMode(CORNER);
 }
 
 function draw() {
-
-	console.log(test);
+  image(background, 0, 0);
+  
+  let characterImg = image(characterObject[0], 0, 0);
 }
+
