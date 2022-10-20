5bc4eb5fddd0f0dc1b88b00d6a421ac2171b32b2
tabo <1005893565@u.icesi.edu.com>
2022-10-20 02:09:10 -0500

    generalizes circle-making function and add sides
    within the scope of the body() function

--- src/hitbox.js
+++ src/hitbox.js
@@ -20,29 +20,67 @@ class Hitbox {
 
 	body() {
 		if(this.check()) {
+			// how many balls fit in the line?
 			let amount = 0;
+			// how long is the line joining the coordinates?
 			let length = 0;
+			// objects corner coordinates
 			let coords = [
 				{x: 0, y: 0},
 				{x: 0, y: 0}
 			];
+
+			// adds points along a line between two dots to the circles array
+			let generateCircles = () => {
+				length = this.distVects(coords);
+				amount = this.ballNum(length);
+				let pAmnt = 1 / amount;
+
+				for (let i = 0; i < amount; i++) {
+					// lerp basically maps the distance between point one
+					// and two and adds a third point at the rate set
+					// by the last value (*--.------*) ~= lerp(1, 2, 0.3)
+					let posX = lerp(coords[0].x, coords[1].x, i * pAmnt);
+					let posY = lerp(coords[0].y, coords[1].y, i * pAmnt);
+					this.balls.push({x: posX, y: posY});
+				}
+
+				// reset vars for the next side
+				amount = 0;
+				length = 0;
+				coords = [
+					{x: 0, y: 0},
+					{x: 0, y: 0}
+				];
+			}
+
 			// up side balls
 			coords = [
 				{x: this.items.x1, y: this.items.y1},
 				{x: this.items.x2, y: this.items.y2}
 			];
+			generateCircles();
 
-			length = this.distVects(coords);
-			amount = this.ballNum(length);
-			let percAmount = 1 / amount;
+			// right side balls
+			coords = [
+				{x: this.items.x2, y: this.items.y2},
+				{x: this.items.x3, y: this.items.y3}
+			];
+			generateCircles();
 
-			for (let i = 0; i < amount; i++) {
-				let posX = lerp(coords[0].x, coords[1].x, i * percAmount);
-				let posY = lerp(coords[0].y, coords[1].y, i * percAmount);
-				this.balls.push({x: posX, y: posY});
-			}
-			console.log(coords, length, amount, percAmount, this.balls);
+			// down side balls
+			coords = [
+				{x: this.items.x3, y: this.items.y3},
+				{x: this.items.x4, y: this.items.y4}
+			];
+			generateCircles();
 
+			// left side balls
+			coords = [
+				{x: this.items.x4, y: this.items.y4},
+				{x: this.items.x1, y: this.items.y1}
+			];
+			generateCircles();
 
 		} else {
 			this.balls.push({x: this.items.center[0], y: this.items.center[1]});
