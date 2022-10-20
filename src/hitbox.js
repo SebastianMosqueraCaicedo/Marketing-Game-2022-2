48f8184ebbd1cb3182646c4753a1a8fc71796a42
tabo <1005893565@u.icesi.edu.com>
2022-10-20 00:17:26 -0500

    create hitbox class
    to get rid of clunky hits for once and for all

---
+++ src/hitbox.js
@@ -0,0 +1,23 @@
+class Hitbox {
+
+	constructor(object) {
+		this.object = object;
+		this.radius = 5;
+		this.balls = [];
+	}
+
+	draw() {
+		// a series of circles along the lines of object's sides
+
+	}
+
+	body() {
+
+	}
+
+	hit(player) {
+		for (let i = 0; i < this.balls.length; i++) {
+
+		}
+	}
+}
